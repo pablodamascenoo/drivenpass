5e38e74dcd8abba25dd44baa5f8333365b8003cb
pablodamascenoo <pablofacex@gmail.com>
2022-07-13 17:29:28 -0300

    feat: created sign-in route

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -6,3 +6,9 @@ export async function signUp(req: Request, res: Response) {
     await authService.signUp(email, password);
     res.sendStatus(200);
 }
+
+export async function signIn(req: Request, res: Response) {
+    const { email, password } = req.body;
+    const token = await authService.signIn(email, password);
+    res.send(token);
+}
--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { signUp } from "../controllers/authController.js";
+import { signIn, signUp } from "../controllers/authController.js";
 import schemaValidator from "../middlewares/schemaValidator.js";
 import userSchema from "../schemas/userSchema.js";
 
 const authRouter = Router();
 
 authRouter.post("/auth/sign-up", schemaValidator(userSchema), signUp);
-authRouter.post("/auth/sign-in", schemaValidator(userSchema));
+authRouter.post("/auth/sign-in", schemaValidator(userSchema), signIn);
 
 export default authRouter;
