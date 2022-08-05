216272f702722106b785686a3d53fd762c866047
Rodrigodcs <rodrigodcsee@gmail.com>
2022-08-05 11:04:00 -0300

    Preparando revisão de código

--- schemas/safetyNoteSchema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-import { SafetyNoteSchemaData } from "../repositories/safetyNoteRepository";
-
-const safetyNoteSchema = Joi.object<SafetyNoteSchemaData>({
-    title: Joi.string().max(50).required(),
-    note: Joi.string().max(1000).required(),
-});
-
-export default safetyNoteSchema;
--- schemas/userSchema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-import { InsertUserData } from "../repositories/userRepository.js";
-
-const userSchema = Joi.object<InsertUserData>({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(10).required(),
-});
-
-export default userSchema;
